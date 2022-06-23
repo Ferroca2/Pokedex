2d5afb6a499560abfd76e2f44363c347348fc827
Ferroca2 <matheus.ferraz@ga.ita.br>
2022-06-23 00:03:58 -0300

    feat: pokedex working

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(poke,index) in pokemons" :key="index">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+import Pokemon from "./components/Pokemon.vue"
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      pokemons: [
+
+      ]
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      this.pokemons = res.data.results
+      console.log(this.pokemons)
+    })
+  },
+  components:{
+    Pokemon
+  }
+}
+</script>
+
+<style>
+
+</style>
